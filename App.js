c670ce5eb17c69cc53e5b0d1e1383ee07074ecd3
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2022-06-02 08:47:14 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,22 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/views/Home';
+import Detalhes from './src/views/Detalhes';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detalhes" component={Detalhes} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
