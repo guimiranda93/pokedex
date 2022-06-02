705c9a9df88668c1fc2e52d16c1f2b425e5c8d4c
Guilherme Miranda <guilhermemiranda.ti@gmail.com>
2022-06-02 09:39:03 -0300

    Finalizacao do projeto

--- src/views/Detalhes/index.js
+++ src/views/Detalhes/index.js
@@ -1,12 +1,76 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import axios from 'axios';
 
-const Detalhes = () => {
+const Detalhes = ({route}) => {
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    if (route.params.pokemon) {
+      getPokemon();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [route.params]);
+
+  const getPokemon = async () => {
+    const retorno = await axios.get(route.params.pokemon.url);
+
+    setPokemon(retorno.data);
+  };
+
   return (
-    <View>
-      <Text>Detalhes</Text>
+    <View style={styles.container}>
+      {pokemon && (
+        <View style={styles.card}>
+          <Image
+            source={{uri: pokemon.sprites.front_default}}
+            style={styles.img}
+          />
+          <Text style={styles.title}>{pokemon.name}</Text>
+          <Text style={styles.subtitle}>Tipo</Text>
+          {pokemon.types.map(item => (
+            <Text style={styles.text}>{item.type.name}</Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
 
 export default Detalhes;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  card: {
+    padding: 30,
+    borderRadius: 5,
+    borderColor: '#ababab',
+    borderWidth: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'black',
+    textTransform: 'capitalize',
+  },
+  img: {
+    width: 200,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 48,
+    color: '#21445B',
+    textTransform: 'capitalize',
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#1A6566',
+  },
+});
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ScrollView, Text, Pressable, StyleSheet} from 'react-native';
+import axios from 'axios';
 
-const Home = () => {
+const Home = ({navigation}) => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    getPokedex();
+  }, []);
+
+  const getPokedex = async () => {
+    const pokemon = await axios.get('https://pokeapi.co/api/v2/pokemon');
+    setPokemons(pokemon.data.results);
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {pokemons.map((item, index) => (
+        <Pressable
+          key={index}
+          onPress={() => navigation.navigate('Detalhes', {pokemon: item})}
+          style={styles.listItem}>
+          <Text style={styles.text}>{item.name}</Text>
+        </Pressable>
+      ))}
+    </ScrollView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listItem: {
+    backgroundColor: 'white',
+    borderColor: '#dedede',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 20,
+  },
+  text: {
+    color: 'black',
+    textTransform: 'capitalize',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
